26ec425164df42a33296050a551c9fda75422ac5
mhudn <mohi81635@gmail.com>
2022-03-11 11:33:52 -0500

    Cypess Automation Test

---
+++ cypress/integration/specs/chaseCredit.spec.js
@@ -0,0 +1,13 @@
+
+/// <reference types="cypress" />
+describe('Chase Credit Card Application', function () {
+    it('Automate chase credit card application', function()
+     {
+        cy.visit("https://www.chase.com")
+        cy.get('[data-id=product-2]').click()
+        cy.xpath('//a[contains(text(),"Personal credit cards")]').click()
+        cy.get('[data-pt-name=cc_freedom_ulim_allcc_name]').contains("Chase Freedom Unlimited").click()
+        cy.get('[id=ApplyNow]').click()
+        cy.url()
+    })
+})
