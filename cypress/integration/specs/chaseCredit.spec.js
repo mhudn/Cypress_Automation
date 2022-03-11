3673f0aad18e06e50f076ba8820034c09d88c07d
Mohi Uddin <mohi@mec.science>
2022-03-11 12:42:11 -0500

    //

--- cypress/integration/specs/chaseCredit.spec.js
+++ cypress/integration/specs/chaseCredit.spec.js
@@ -8,6 +8,6 @@ describe('Chase Credit Card Application', function () {
         cy.xpath('//a[contains(text(),"Personal credit cards")]').click()
         cy.get('[data-pt-name=cc_freedom_ulim_allcc_name]').contains("Chase Freedom Unlimited").click()
         cy.get('[id=ApplyNow]').click()
-        cy.url()
+        //cy.url()
     })
 })
